64d89ee71a89d712eb2342ec28936b447b6f2e2d
eugeneLitv <eugeneLitv@users.noreply.github.com>
2016-04-08 16:44:32 +0300

    ClientTest.java: add tests, remove @Ignore

--- eugeneL/BankApplication/test/com/acme/bankapp/ClientTest.java
+++ eugeneL/BankApplication/test/com/acme/bankapp/ClientTest.java
@@ -4,9 +4,52 @@
  */
 package com.acme.bankapp;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore("ClientTest ignored because it is empty currently")
+import static org.junit.Assert.assertEquals;
+
 public class ClientTest {
+    final static private float delta = 0.001f;
+    final static private float defaultOverdraft = 300f; // must be the same as in class Client
+    final static private String defaultClientName = "Unnamed Client"; // must be the same as in class Client
+
+    @Test
+    public void createClientDefault() {
+        final Client client;
+
+        client = new Client();
+        assertEquals("Client(): Client Name incorrect", defaultClientName, client.getName());
+        assertEquals("Client(): initialOverdraft incorrect", defaultOverdraft, client.getInitialOverdraft(), delta);
+    }
+
+    @Test
+    public void createClientWithNameAndOverdraft() {
+        final String name = "Test Client Name";
+        final float initialOverdraft = 100.34f;
+        final Client client;
+
+        client = new Client(name, initialOverdraft);
+        assertEquals("Client(name, initialOverdraft): Client Name incorrect", name, client.getName());
+        assertEquals("Client(name, initialOverdraft): initialOverdraft incorrect", initialOverdraft, client.getInitialOverdraft(), delta);
+    }
+
+     @Test
+    public void createClientWithName() {
+        final String name = "Test Client Name";
+        final Client client;
+
+        client = new Client(name);
+        assertEquals("Client(name): Client Name incorrect", name, client.getName());
+        assertEquals("Client(name): initialOverdraft incorrect", defaultOverdraft, client.getInitialOverdraft(), delta);
+    }
+
+     @Test
+    public void createClientWithOverdraft() {
+        final float initialOverdraft = 100.34f;
+        final Client client;
+
+        client = new Client(initialOverdraft);
+        assertEquals("Client(initialOverdraft): Client Name incorrect", defaultClientName, client.getName());
+        assertEquals("Client(initialOverdraft): initialOverdraft incorrect", initialOverdraft, client.getInitialOverdraft(), delta);
+    }
 }
