2501c97d7d2ffdb20ddf680f7af17a2bf3c204aa
eugeneLitv <eugeneLitv@users.noreply.github.com>
2016-04-15 15:44:49 +0300

    Simplify Enum

--- eugeneL/BankApplication/src/main/java/com/acme/bankapp/AccountTypes.java
+++ eugeneL/BankApplication/src/main/java/com/acme/bankapp/AccountTypes.java
@@ -4,17 +4,4 @@
  */
 package com.acme.bankapp;
 
-enum AccountTypes {
-    CHECKING("Checking"),
-    SAVING("Saving");
-
-    private final String name;
-
-    AccountTypes(String n) {
-        name = n;
-    }
-
-    public String getAccountTypeName() {
-        return name;
-    }
-}
+enum AccountTypes {CHECKING, SAVING;}
